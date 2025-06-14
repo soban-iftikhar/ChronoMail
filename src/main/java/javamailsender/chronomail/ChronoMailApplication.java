18c03bd82c8074c9cb6fc2addd89250ec73d7a41
Soban Iftikhar <sobaniftikhar1098@gmail.com>
2025-06-14 23:33:14 +0500

    Initial commit - ChronoMail core structure

---
+++ src/main/java/javamailsender/chronomail/ChronoMailApplication.java
@@ -0,0 +1,13 @@
+package javamailsender.chronomail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChronoMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChronoMailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/javamailsender/chronomail/ChronoMailApplicationTests.java
@@ -0,0 +1,13 @@
+package javamailsender.chronomail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChronoMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
